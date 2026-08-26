e82ae311e14b8cae496c79e75d158e6b5c0b98f1
Marcela Maciel <macielwebcode@gmail.com>
2026-08-26 13:06:54 -0300

    inicio estilizando admin

--- components/PostListAdmin/index.tsx
+++ components/PostListAdmin/index.tsx
@@ -1,4 +1,7 @@
 import { findPostByIdAdminPrivate } from "@/lib/post/queries/admin"
+import clsx from "clsx"
+import { Trash2Icon } from "lucide-react"
+import Link from "next/link"
 
 
 export default async function PostListAdmin(){
@@ -7,7 +10,25 @@ export default async function PostListAdmin(){
     return(
         <>
             {posts.map(post => {
-                return <p key={post.id}>{post.title}</p>
+                return <div 
+                    className={clsx('py-2 px-2', 
+                    !post.published && 'bg-slate-300', 
+                    'flex gap-2 items-center justify-between'
+                    )} key={post.id}>
+                        
+                    <Link href={`/admin/post/${post.id}`}>{post.title}</Link>
+
+                    {!post.published && (
+                        <span className='text-xs
+                         text-slate-600 
+                         italic'>
+                        Não publicado</span>
+                    )}
+
+                    <button>
+                        <Trash2Icon size={18} />
+                    </button>
+                </div>
             })}
         </>
     )
